77271a6a5f04d71dff09d509e0dcbb50080fa787
Viddi <viddi@nplexity.com>
2014-07-30 21:33:32 -0400

    Fixed dummy.py tabbing

--- python/dummy.py
+++ python/dummy.py
@@ -3,22 +3,22 @@
 /*https://github.com/django/django/blob/master/django/core/cache/backends/dummy.py*/
 
 def delete(self, key, version=None):
-   key = self.make_key(key, version=version)
-   self.validate_key(key)
+    key = self.make_key(key, version=version)
+    self.validate_key(key)
 
 def get_many(self, keys, version=None):
-   return {}
+    return {}
 
 def has_key(self, key, version=None):
-   key = self.make_key(key, version=version)
-   self.validate_key(key)
-   return False
+    key = self.make_key(key, version=version)
+    self.validate_key(key)
+    return False
 
 def set_many(self, data, timeout=DEFAULT_TIMEOUT, version=None):
-   pass
+    pass
 
 def delete_many(self, keys, version=None):
-   pass
+    pass
 
 def clear(self):
-   pass
+    pass
